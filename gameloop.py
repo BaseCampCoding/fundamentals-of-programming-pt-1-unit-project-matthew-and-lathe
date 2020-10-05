9fdef9d06eba5c0698aece3551e7c34ee070dc96
Lathe Ward <lward21@basecampcodingacademy.org>
2020-10-05 16:02:31 -0500

    imported cards

---
+++ gameloop.py
@@ -0,0 +1,3 @@
+import cards
+
+cards.makeNewDeck()
--- greeting.py
+++ greeting.py
@@ -13,3 +13,4 @@ mission_statement = """You have entered the casino with $100.
 Your goal is to turn that into $1000 dollars by playing blackjack.
 Good luck.
 """
+print(mission_statement)
