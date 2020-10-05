d5c9b4ad3946eecb386afaf20a017ec42bf965a0
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-10-05 15:45:12 -0500

    finished docstring

--- cards.py
+++ cards.py
@@ -8,6 +8,9 @@ class Card ():
 
 
 def makeNewDeck () -> list:
+    """This functions takes in no data, but returns a shuffled deck, in which is stored
+    as a 2D List, with the first element of every sublist being the suit, and the second
+    element being the rank, both being strings"""
     RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     SUITS = ['♣', '♦', '♥', '♠']
     newDeck = []
