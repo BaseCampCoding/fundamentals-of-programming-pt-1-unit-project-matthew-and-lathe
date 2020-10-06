7ca3af6c38f292effe64f1452096e769899d650d
Lathe Ward <lward21@basecampcodingacademy.org>
2020-10-06 09:00:04 -0500

    further developed 'main_game' function

--- gameloop.py
+++ gameloop.py
@@ -5,3 +5,6 @@ def main_game(money, minumum_bet):
     newdeck = cards.makeNewDeck()
     current_card = newdeck.pop()
     cards.printCard(current_card)
+    hands = []
+    hands.append(newdeck.pop())
+    hands.append(newdeck.pop())
