66f08db36ac2302e5f66f8751fd514cce0d02173
Lathe Ward <lward21@basecampcodingacademy.org>
2020-10-06 10:11:47 -0500

    Added hit or stand option in gameloop

--- gameloop.py
+++ gameloop.py
@@ -1,16 +1,18 @@
 import cards
 
+
 def show_cards(d_hand: list, p_hand: list) -> None:
     """Simply prints out the player and dealer's hands"""
     card_split = [card.split("\n") for card in d_hand]
-    zipped = zip(*card_split)    
+    zipped = zip(*card_split)
     for elems in zipped:
         print("".join(elems))
     print()
     card_split = [card.split("\n") for card in p_hand]
-    zipped = zip(*card_split)    
+    zipped = zip(*card_split)
     for elems in zipped:
         print("".join(elems))
+
 
 def play_round():
     new_deck = cards.make_new_deck()
@@ -26,10 +28,12 @@ def play_round():
         current_card = new_deck.pop()
         dealer_hands.append(current_card)
         display_dealer_hands.append(cards.print_card([]))
-        
+
     show_cards(display_dealer_hands, display_hands)
 
-    # for card in display_dealer_hands:
-    #     print(card, sep='')  
-    
-play_round()
+    print("-------------")
+    print("Hit or Stand?")
+    print("-------------")
+    player_choice = input()
+    while player_choice.lower() != "hit" and player_choice.lower() != "stand":
+        print("Please enter hit or stand.")
