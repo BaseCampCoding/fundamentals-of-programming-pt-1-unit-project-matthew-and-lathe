04d06fbb1f7aace924b65bf9f2ba46393d07e981
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-10-06 10:35:47 -0500

    modified the loop in play_round, so that now, the player can hit and stand

--- gameloop.py
+++ gameloop.py
@@ -13,6 +13,14 @@ def show_cards(d_hand: list, p_hand: list) -> None:
     for elems in zipped:
         print("".join(elems))
 
+def add_to_hand(new_deck: list, hands: list, display_hands: list, is_player: bool) -> None:
+    current_card = new_deck.pop()
+    if is_player == True:
+        hands.append(current_card)
+        display_hands.append(cards.print_card(current_card))
+    else:
+        hands.append(current_card)
+        display_hands.append(cards.print_card([]))
 
 def play_round():
     new_deck = cards.make_new_deck()
@@ -21,24 +29,24 @@ def play_round():
     dealer_hands = []
     display_dealer_hands = []
     for i in range(2):
-        current_card = new_deck.pop()
-        hands.append(current_card)
-        display_hands.append(cards.print_card(current_card))
+        add_to_hand(new_deck, hands, display_hands, True)
+        add_to_hand(new_deck, dealer_hands, display_dealer_hands, False)
 
-        current_card = new_deck.pop()
-        dealer_hands.append(current_card)
-        display_dealer_hands.append(cards.print_card([]))
+    hand_value = 0
+    while hand_value <= 21:
+        show_cards(display_dealer_hands, display_hands)
+        print("-------------")
+        print("Hit or Stand?")
+        print("-------------")
+        player_choice = input()
+        player_choice = player_choice.lower()
+        while player_choice != "hit" and player_choice != "stand":
+            print("Please enter hit or stand.")
+        if player_choice == "hit":
+            add_to_hand(new_deck, hands, display_hands, True)
+            hand_value = cards.get_hand_value(hands)
+        if player_choice == "stand":
+            break
+    print(f"Hand's Value: {hand_value}")
 
-    show_cards(display_dealer_hands, display_hands)
-
-    print("-------------")
-    print("Hit or Stand?")
-    print("-------------")
-    player_choice = input()
-    player_choice = player_choice.lower()
-    while player_choice != "hit" and player_choice != "stand":
-        print("Please enter hit or stand.")
-    if player_choice == "hit":
-        print()  # here goes function
-    if player_choice == "stand":
-        hand_value = cards.get_hand_value(hands)
+play_round()
