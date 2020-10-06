646bdc317656b8e164a3df18b23b07e015aca9dd
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-10-06 09:00:54 -0500

    changed the print_card function

--- blackjack.py
+++ blackjack.py
@@ -3,3 +3,6 @@ import doclear
 import cards
 import greeting
 import gameloop
+
+while True:
+    choice = input("Enter '0' to exit")
--- cards.py
+++ cards.py
@@ -1,19 +1,19 @@
 from random import shuffle
 # ♣, ♦, ♥, ♠
-def makeNewDeck () -> list:
+def make_new_deck () -> list:
     """This function takes in no data, but returns a shuffled deck, in which is stored
     as a 2D List, with the first element of every sublist being the suit, and the second
     element being the rank, both being strings"""
     RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     SUITS = ['♣', '♦', '♥', '♠']
-    newDeck = []
+    new_deck = []
     for s in SUITS:
         for r in RANKS:
-            newDeck.append([s, r])
-    shuffle(newDeck)
-    return newDeck
+            new_deck.append([s, r])
+    shuffle(new_deck)
+    return new_deck
 
-def printCard (card: list) -> None:
+def print_card (card: list) -> str:
     """This function prints the specially formatted cards
     >>> printCard(['♠', 'A'])
     #####
@@ -23,11 +23,21 @@ def printCard (card: list) -> None:
     #####
     
     """
-    newString = f"""
-    #####
-    #{card[1]}  #
-    #   #
-    #  {card[0]}#
-    #####
-    """
-    print(newString)
+    new_string = ""
+    if not card:
+        new_string = """
+        #####
+        #+++#
+        #+++#
+        #+++#
+        #####
+        """
+    else:
+        new_string = f"""
+        #####
+        #{card[1]}  #
+        #   #
+        #  {card[0]}#
+        #####
+        """
+    return new_string
