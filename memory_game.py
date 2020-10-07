7f7e893406495208eb10d18f5d6eb327f6bcae87
Lathe Ward <lward21@basecampcodingacademy.org>
2020-10-07 11:15:57 -0500

    Added the shuffle section of memory game

--- memory_game.py
+++ memory_game.py
@@ -1,4 +1,5 @@
 import random
+
 # ♣, ♦, ♥, ♠
 
 print(
@@ -8,41 +9,54 @@ print(
     See if you can remember them all.
     """
 )
-cardset_1= ["""
+
+
+cardset_1 = [
+    """
     ##### 
     #A  # 
     #   # 
     #  ♠# 
     #####  
     """,
-    "ace of spades"]
+    "ace of spades",
+]
 
-cardset_2 = ["""
+cardset_2 = [
+    """
     #####
     #K  #
     #   #
     #  ♥#
     #####
-    """
-    , "king of hearts"]
+    """,
+    "king of hearts",
+]
 
-cardset_3 = ["""
+cardset_3 = [
+    """
     #####
     #Q  #
     #   #
     #  ♣#
     #####
-    """
-    , "queen of clubs"]
+    """,
+    "queen of clubs",
+]
 
-cardset_4_art = """
+cardset_4 = [
+    """
     #####
     #J  #
     #   #
     #  ♦#
     #####
     """,
-"jack of diamonds"]
+    "jack of diamonds",
+]
 
-
+memory_cards = [cardset_1, cardset_2, cardset_3, cardset_4]
+shuffle_cards1 = random.choice(memory_cards)
+shuffle_cards2 = random.choice(memory_cards)
+print(shuffle_cards1[0], shuffle_cards2[0])
 question = input("What two cards did you see?")
