cf6ebf1feb22b4b1efe65c7f846935edc4e8635e
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-10-07 09:37:48 -0500

    added the full betting system, and a way to exit game

--- blackjack.py
+++ blackjack.py
@@ -5,24 +5,41 @@ import greeting
 import gameloop
 
 difficulty = 0
+money = 100
 greeting.hello_player()
-difficulty = greeting.choose_dealer()
-doclear.clear()
-while True:
-    status = gameloop.play_round(difficulty)
-    if status == 0:
-        print("You won!")
-    elif status == 1:
-        print("You lost!")
-    else:
-        print("It was a tie!")
-    choice = input("Enter '0' to exit or P to play again: ")
-    choice = choice.lower()
-    if choice == "0":
+choice = ''
+while money >= 10:
+    difficulty = greeting.choose_dealer(money)
+    if difficulty == 4:
         break
-    elif choice.lower() == "p":
-        doclear.clear()
-        continue
-    else:
-        doclear.clear()
-        continue
+    doclear.clear()
+    while choice != 'leave' and money >= 10:
+        min_bet = 0
+        if difficulty == 0:
+            min_bet = 10
+        elif difficulty == 1:
+            min_bet = 25
+        elif difficulty == 2:
+            min_bet = 50
+        else:
+            min_bet = 100
+        bet = greeting.get_bet(money, min_bet)
+        status = gameloop.play_round(difficulty)
+        if status == 0:
+            print("You won!")
+            money += bet * 2
+        elif status == 1:
+            print("You lost!")
+            money -= bet
+        else:
+            print("It was a tie!")
+        choice = input("Enter 'leave' to pick another table or 'again' to play again: ")
+        choice = choice.lower()
+        while choice != 'leave' and choice != 'again':
+            print("Invalid input.")
+            choice = input("Enter 'leave' to pick another table or 'again' to play again: ")
+            choice = choice.lower()
+if money < 10:
+    print("Get outta here!")
+else:
+    print("See ya later, then.")
--- greeting.py
+++ greeting.py
@@ -23,13 +23,14 @@ def hello_player():
     doclear.clear()
 
 
-def choose_dealer():
+def choose_dealer(money: int):
     print(table_string)
     characters = """
     Billy is the youngest dealer, far from the best at cards. He is also a loud mouth. (Min Bet: 10)
     Karen is one of the better ones, moderately good. Not as talkative as Billy, but not scared to chit-chat. (Min Bet: 25)
     Lathe is the most handsome of the group, but not quite the best. He is usually pretty quiet. (Min Bet: 50)
     Matt: The man, the myth, the legend himself. Need I say more? (Min Bet: 100)
+    Once you have $1000, you should leave
     """
     print(characters)
     difficulty_selection = input("Choose your dealer with their corresponding number:")
@@ -38,10 +39,25 @@ def choose_dealer():
     while doclear.RepresentsInt(difficulty_selection) == False or (
         difficulty_selection < 0 or difficulty_selection > 3
     ):
-        print("Please enter value between 0-3")
+        print("Please enter value between 0-3, and a dealer with a minimum bet you can meet.")
         difficulty_selection = input(
             "Choose your dealer with their corresponding number:"
         )
+
+        #these if-elif s are for making sure the player meets the minimum bet,
+        #or for them to leave the casino
+        if difficulty_selection == 0 and money < 10:
+            difficulty_selection = -1
+        elif difficulty_selection == 1 and money < 25:
+            difficulty_selection = -1
+        elif difficulty_selection == 2 and money < 50:
+            difficulty_selection = -1
+        elif difficulty_selection == 3 and money < 100:
+            difficulty_selection = -1
+        elif difficulty_selection == 4 and money >= 1000:
+            break
+        elif difficulty_selection == 4 and money < 1000:
+            print("Not yet, you have got to get to $1000 first!")
     if difficulty_selection == (0):
         print("[Billy: Get ready to eat it!]")
     if difficulty_selection == (1):
@@ -52,5 +68,16 @@ def choose_dealer():
         print("[Lathe: I'm not conceited. Conceit is a fault and I have no faults.]")
     if difficulty_selection == (3):
         print("[Matt: So you have chosen death.]")
-
+    if difficulty_selection == (4):
+        print("Thanks for coming, hope to see you again!")
     return difficulty_selection
+
+def get_bet (money: int, min_bet: int) -> int:
+    print(f"You have ${money}")
+    bet = input(f"Please enter a bet of atleast ${min_bet}")
+    is_int = doclear.RepresentsInt(bet)
+    while is_int == False or int(bet) > min_bet:
+        print("Invalid input.")
+        bet = input(f"Please enter a bet of atleast ${min_bet}")
+        is_int = doclear.RepresentsInt(bet)
+    return bet
