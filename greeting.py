61cbe9429d736dc259c7036317a82c36dcc74e49
Lathe Ward <lward21@basecampcodingacademy.org>
2020-10-08 10:39:46 -0500

    Added colorization to menu

--- greeting.py
+++ greeting.py
@@ -1,5 +1,6 @@
 import doclear
 from tableart import table_string
+from cards import colored_text
 
 
 def hello_player():
@@ -12,13 +13,13 @@ def hello_player():
     #     #  #        #     #  #        #    #     #    #    #     #  #        #    #
     #####    #######  #     #  #######  #     #    ######    #     #  #######  #     #    
     """
-    print(bigwelcome)
+    colored_text(bigwelcome, 0)
     mission_statement = """
     You have entered the casino with $100.
     Your goal is to turn that into $1000 dollars by playing blackjack.
     Good luck.
     """
-    print(mission_statement)
+    colored_text(mission_statement, 0)
     input("Press any key to play ")
     doclear.clear()
 
@@ -47,15 +48,21 @@ def choose_dealer(money: int):
     elif difficulty_selection == 4 and money < 1000:
         print("Not yet, you have got to get to $1000 first!")
         difficulty_selection = -1
-    while doclear.RepresentsInt(difficulty_selection) == False or (difficulty_selection < 0 or difficulty_selection > 3):
-        print("Please enter value between 0-3, and a dealer with a minimum bet you can meet.")
-        difficulty_selection = input("Choose your dealer with their corresponding number:")
+    while doclear.RepresentsInt(difficulty_selection) == False or (
+        difficulty_selection < 0 or difficulty_selection > 3
+    ):
+        print(
+            "Please enter value between 0-3, and a dealer with a minimum bet you can meet."
+        )
+        difficulty_selection = input(
+            "Choose your dealer with their corresponding number:"
+        )
         if doclear.RepresentsInt(difficulty_selection) == True:
             difficulty_selection = int(difficulty_selection)
         else:
             continue
-        #these if-elif s are for making sure the player meets the minimum bet,
-        #or for them to leave the casino
+        # these if-elif s are for making sure the player meets the minimum bet,
+        # or for them to leave the casino
         if difficulty_selection == 0 and money < 10:
             difficulty_selection = -1
         elif difficulty_selection == 1 and money < 25:
@@ -69,20 +76,24 @@ def choose_dealer(money: int):
         elif difficulty_selection == 4 and money < 1000:
             print("Not yet, you have got to get to $1000 first!")
     if difficulty_selection == (0):
-        print("[Billy: Get ready to eat it!]")
+        colored_text("[Billy: Get ready to eat it!]", 2)
     if difficulty_selection == (1):
-        print(
-            "[Karen: I’m sorry that people are so jealous of me but I can’t help it that I’m popular.]"
+        colored_text(
+            "[Karen: I’m sorry that people are so jealous of me but I can’t help it that I’m popular.]",
+            4,
         )
     if difficulty_selection == (2):
-        print("[Lathe: I'm not conceited. Conceit is a fault and I have no faults.]")
+        colored_text(
+            "[Lathe: I'm not conceited. Conceit is a fault and I have no faults.]", 0
+        )
     if difficulty_selection == (3):
-        print("[Matt: So you have chosen death.]")
+        colored_text("[Matt: So you have chosen death.]", 1)
     if difficulty_selection == (4):
-        print("Thanks for coming, hope to see you again!")
+        colored_text("Thanks for coming, hope to see you again!", 4)
     return difficulty_selection
 
-def get_bet (money: int, min_bet: int) -> int:
+
+def get_bet(money: int, min_bet: int) -> int:
     print(f"You have ${money}")
     bet = input(f"Please enter a bet of atleast ${min_bet}: ")
     is_int = doclear.RepresentsInt(bet)
@@ -93,7 +104,8 @@ def get_bet (money: int, min_bet: int) -> int:
     bet = int(bet)
     return bet
 
-def choose_game () -> str:
+
+def choose_game() -> str:
     print("What game do you want to play? Enter the name of the game.")
     choice = input("blackjack, minipoker, or memory? ")
     while choice != "blackjack" and choice != "minipoker" and choice != "memory":
