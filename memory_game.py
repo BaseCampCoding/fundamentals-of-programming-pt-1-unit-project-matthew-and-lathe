f7f45db807d85bec5d150971be3b1430f049c34e
Lathe Ward <lward21@basecampcodingacademy.org>
2020-10-08 08:58:01 -0500

    Added hard mode by showing 4 cards instead of 2

--- memory_game.py
+++ memory_game.py
@@ -88,3 +88,52 @@ def main_memory_game(difficulty: int):
             return 0
         else:
             return 1
+    if difficulty == 2 or difficulty == 3:
+        print("Here we go...")
+        sleep(2)
+        print(
+            shuffle_cards1[0], shuffle_cards2[0], shuffle_cards3[0], shuffle_cards4[0]
+        )
+        sleep(2)
+        doclear.clear()
+        print("[Example: ace of spades, queen of hearts]")
+        question1 = input("What was the first card you saw? ")
+        question1 = question1.lower()
+        if question1 == shuffle_cards1[1]:
+            print("You got it!")
+        if question1 != shuffle_cards1[1]:
+            print("Nope!")
+
+        question2 = input("What was the second card you saw? ")
+        print("[Example: ace of spades, queen of hearts]")
+        question2 = question2.lower()
+        if question2 == shuffle_cards2[1]:
+            print("You got it!")
+        if question2 != shuffle_cards2[1]:
+            print("Nope!")
+
+        question3 = input("What was the third card you saw? ")
+        print("[Example: ace of spades, queen of hearts]")
+        question3 = question3.lower()
+        if question3 == shuffle_cards3[1]:
+            print("You got it!")
+        if question3 != shuffle_cards3[1]:
+            print("Nope!")
+
+        question4 = input("What was the fourth card you saw? ")
+        print("[Example: ace of spades, queen of hearts]")
+        question4 = question4.lower()
+        if question4 == shuffle_cards4[1]:
+            print("You got it!")
+        if question4 != shuffle_cards4[1]:
+            print("Nope!")
+
+        if (
+            question1 == shuffle_cards1[1]
+            and question2 == shuffle_cards2[1]
+            and question3 == shuffle_cards3[1]
+            and question4 == shuffle_cards4[1]
+        ):
+            return 0
+        else:
+            return 1
